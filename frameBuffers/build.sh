3634913ce61f1ac9cb66a4ed48e5d7bdb3072acb
AysegulTerzi <terziaysegul9696.com>
2023-09-13 08:35:45 +0000

    chmod

--- frameBuffers/build.sh
+++ frameBuffers/build.sh
@@ -3,6 +3,10 @@
 CC=g++
 CPPFLAGS="-I../Include"
 LDFLAGS=`pkg-config --libs glew `
-LDFLAGS="$LDFLAGS -lglut"
+LDFLAGS="$LDFLAGS -lglut -lGLEW -lGL"
+
+chmod +x build.sh
 
 $CC bindframe.cpp $CPPFLAGS $LDFLAGS -o bindframe
+
+
